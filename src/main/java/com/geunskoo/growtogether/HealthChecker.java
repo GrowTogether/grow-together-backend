33191cd63a5fb69b199b76a2a2c4025d433f1fdf
김태근 <oryukdo3@pusan.ac.kr>
2024-08-02 20:05:03 +0900

    fix: health check url 수정

--- src/main/java/com/geunskoo/growtogether/HealthChecker.java
+++ src/main/java/com/geunskoo/growtogether/HealthChecker.java
@@ -1,13 +1,20 @@
 package com.geunskoo.growtogether;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
+@RequiredArgsConstructor
 public class HealthChecker {
 
-    @GetMapping("/health")
-    public String healthChecker(){
-        return "Hello World!";
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String getProfile(){
+        return Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
     }
 }
