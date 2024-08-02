f917692ef9630165f0b70653290dbb3a484fca4d
김태근 <oryukdo3@pusan.ac.kr>
2024-08-03 00:03:31 +0900

    fix: build시 테스트 오류 개선

--- src/test/java/com/geunskoo/growtogether/GrowTogetherApplicationTests.java
+++ src/test/java/com/geunskoo/growtogether/GrowTogetherApplicationTests.java
@@ -3,7 +3,7 @@ package com.geunskoo.growtogether;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GrowTogetherApplicationTests.class)
 class GrowTogetherApplicationTests {
 
     @Test
